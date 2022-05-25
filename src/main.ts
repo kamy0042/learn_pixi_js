55bcdb2383a7d18739b21a17384ee47ab356d069
kamy18112 <kamahori.dev@gmail.com>
2022-05-26 00:14:27 +0900

    処理を関数にまとめる

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import './style.css'
 import * as PIXI from "pixi.js"
 
-const app = new PIXI.Application( { backgroundColor: 0x003333 } );
-document.body.appendChild( app.view );
-const textStyle = new PIXI.TextStyle( { fill: 0xffffff } )
-const titleText = new PIXI.Text( "Hello World!", textStyle );
-titleText.position.set( 350, 300 );
-app.ticker.add( delta => {
-    titleText.rotation += 0.05 * ( 1 + delta );
-} );
-app.stage.addChild( titleText );
+const createApp = () => {
+    const app = new PIXI.Application( { backgroundColor: 0x003333 } );
+    document.body.appendChild( app.view );
+    const textStyle = new PIXI.TextStyle( { fill: 0xffffff } )
+    const titleText = new PIXI.Text( "Hello World!", textStyle );
+    titleText.position.set( 350, 300 );
+    app.ticker.add( delta => {
+        titleText.rotation += 0.05 * ( 1 + delta );
+    } );
+    app.stage.addChild( titleText );
+
+    return app;
+}
+
+const app = createApp();
