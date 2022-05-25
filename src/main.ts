7b924ee8f7a0fda63097bbdb94fe925792139087
kamy18112 <kamahori.dev@gmail.com>
2022-05-26 00:09:42 +0900

    テキストを動かしてみる

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,8 @@ const app = new PIXI.Application( { backgroundColor: 0x003333 } );
 document.body.appendChild( app.view );
 const textStyle = new PIXI.TextStyle( { fill: 0xffffff } )
 const titleText = new PIXI.Text( "Hello World!", textStyle );
+titleText.position.set( 350, 300 );
+app.ticker.add( delta => {
+    titleText.rotation += 0.05 * ( 1 + delta );
+} );
 app.stage.addChild( titleText );
