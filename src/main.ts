7e0543b1ab52e87eac945e18514be6bcbcf33eef
kamy18112 <kamahori.dev@gmail.com>
2022-05-27 17:02:09 +0900

    ドラッグ用のイベントハンドラー追加

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,31 @@ const createApp = () => {
     const textStyle = new PIXI.TextStyle( { fill: 0xffffff } )
     const titleText = new PIXI.Text( "Hello World!", textStyle );
 
+    const onDragStart = (event) => {
+        this.data = event.data;
+        this.alpha = 0.5;
+        this.dragging = true;
+    }
+
+    const onDragEnd = () => {
+        this.alpha = 1;
+        this.dragging = false;
+        // set the interaction data to null
+        this.data = null;
+    }
+
+    const onDragMove = () => {
+        if (this.dragging) {
+            const newPosition = this.data.getLocalPosition(this.parent);
+            this.x = newPosition.x;
+            this.y = newPosition.y;
+        }
+    }
+
     titleText.position.set( 350, 300 );
     titleText.anchor.set(0.5);
+    titleText.interactive = true;
+    titleText.buttonMode = true;
 
     app.ticker.add( delta => {
         titleText.rotation += 0.005 * ( 1 + delta );
