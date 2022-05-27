770dcb88a0e0bc5e4020f0b898924b13141abdfc
kamy18112 <kamahori.dev@gmail.com>
2022-05-27 17:32:56 +0900

    処理を責務ごとに分割する

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,26 @@
 import './style.css'
 import * as PIXI from "pixi.js"
 
-const createApp = async () => {
+const createApp =  () => {
+    const app = new PIXI.Application( { backgroundColor: 0x003333 } );
+    document.body.appendChild( app.view );
+
+    return app;
+}
+
+const createText = () => {
+    const textStyle = new PIXI.TextStyle( { fill: 0xffffff } )
+    const titleText = new PIXI.Text( "Hello World!", textStyle );
+    titleText.position.set( 350, 300 );
+    titleText.anchor.set(0.5);
+
+    return titleText
+}
+
+const createDraggableLogo = async () => {
+    const texture = await PIXI.Texture.fromURL('/src/assets/favicon.svg')
+    const logo = new PIXI.Sprite(texture);
+
     function onDragStart(event) {
         this.data = event.data;
         this.alpha = 0.5;
@@ -22,16 +41,6 @@ const createApp = async () => {
         }
     }
 
-    const app = new PIXI.Application( { backgroundColor: 0x003333 } );
-    document.body.appendChild( app.view );
-    const textStyle = new PIXI.TextStyle( { fill: 0xffffff } )
-    const titleText = new PIXI.Text( "Hello World!", textStyle );
-    const texture = await PIXI.Texture.fromURL('/src/assets/favicon.svg')
-    const logo = new PIXI.Sprite(texture);
-
-    titleText.position.set( 350, 300 );
-    titleText.anchor.set(0.5);
-
     logo.position.set( 250, 200 );
     logo.anchor.set(0.5);
     logo.interactive = true;
@@ -42,13 +51,18 @@ const createApp = async () => {
         .on('pointerupoutside', onDragEnd)
         .on('pointermove', onDragMove);
 
-
-    app.ticker.add( delta => {
-        titleText.rotation += 0.005 * ( 1 + delta );
-    } );
-    app.stage.addChild( titleText,logo );
-
-    return app;
+    return logo;
 }
 
-const app = createApp();
+const main = async () => {
+    const app = createApp()
+    const text = createText();
+    const logo = await createDraggableLogo();
+
+    app.ticker.add( delta => {
+        text.rotation += 0.005 * ( 1 + delta );
+    } );
+    app.stage.addChild( text, logo );
+}
+
+main();
