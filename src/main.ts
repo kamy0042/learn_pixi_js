58902d5035dee911c0a4d7a76104f84a73747a2e
kamy18112 <kamahori.dev@gmail.com>
2022-05-27 17:25:02 +0900

    ロゴをドラッグできるよう変更

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,20 @@
 import './style.css'
 import * as PIXI from "pixi.js"
 
-const createApp = () => {
-    const app = new PIXI.Application( { backgroundColor: 0x003333 } );
-    document.body.appendChild( app.view );
-    const textStyle = new PIXI.TextStyle( { fill: 0xffffff } )
-    const titleText = new PIXI.Text( "Hello World!", textStyle );
-
-    const onDragStart = (event) => {
+const createApp = async () => {
+    function onDragStart(event) {
         this.data = event.data;
         this.alpha = 0.5;
         this.dragging = true;
     }
 
-    const onDragEnd = () => {
+    function onDragEnd() {
         this.alpha = 1;
         this.dragging = false;
-        // set the interaction data to null
         this.data = null;
     }
 
-    const onDragMove = () => {
+    function onDragMove() {
         if (this.dragging) {
             const newPosition = this.data.getLocalPosition(this.parent);
             this.x = newPosition.x;
@@ -28,15 +22,31 @@ const createApp = () => {
         }
     }
 
+    const app = new PIXI.Application( { backgroundColor: 0x003333 } );
+    document.body.appendChild( app.view );
+    const textStyle = new PIXI.TextStyle( { fill: 0xffffff } )
+    const titleText = new PIXI.Text( "Hello World!", textStyle );
+    const texture = await PIXI.Texture.fromURL('/src/assets/favicon.svg')
+    const logo = new PIXI.Sprite(texture);
+
     titleText.position.set( 350, 300 );
     titleText.anchor.set(0.5);
-    titleText.interactive = true;
-    titleText.buttonMode = true;
+
+    logo.position.set( 250, 200 );
+    logo.anchor.set(0.5);
+    logo.interactive = true;
+    logo.buttonMode = true;
+    logo
+        .on('pointerdown', onDragStart)
+        .on('pointerup', onDragEnd)
+        .on('pointerupoutside', onDragEnd)
+        .on('pointermove', onDragMove);
+
 
     app.ticker.add( delta => {
         titleText.rotation += 0.005 * ( 1 + delta );
     } );
-    app.stage.addChild( titleText );
+    app.stage.addChild( titleText,logo );
 
     return app;
 }
