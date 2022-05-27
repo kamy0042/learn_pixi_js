07b57c2b97d2cb9c0c133f955f95e4f1012e7b49
kamy18112 <kamahori.dev@gmail.com>
2022-05-27 16:59:43 +0900

    テキストをドラッグしやすくする

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ const createApp = () => {
     document.body.appendChild( app.view );
     const textStyle = new PIXI.TextStyle( { fill: 0xffffff } )
     const titleText = new PIXI.Text( "Hello World!", textStyle );
+
     titleText.position.set( 350, 300 );
+    titleText.anchor.set(0.5);
+
     app.ticker.add( delta => {
-        titleText.rotation += 0.05 * ( 1 + delta );
+        titleText.rotation += 0.005 * ( 1 + delta );
     } );
     app.stage.addChild( titleText );
 
